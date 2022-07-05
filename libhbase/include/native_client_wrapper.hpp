91b67bb34daa43c2d08d3b02a7407878c0876113
HaNeul-Kim <hskimsky@gmail.com>
2022-07-05 22:27:56 +0900

    document 주석 추가

--- libhbase/include/native_client_wrapper.hpp
+++ libhbase/include/native_client_wrapper.hpp
@@ -5,8 +5,26 @@ using namespace std;
 
 void print_value(int x);
 
+/**
+ * rowkey 를 지정하여 get 한다.
+ *
+ * @param rowkeys rowkey 목록
+ */
 void gets(vector<string> *rowkeys);
 
+/**
+ * rowkey, column family 를 지정하여 get 한다.
+ *
+ * @param rowkeys rowkey 목록
+ * @param families column family 목록
+ */
 void gets(vector<string> *rowkeys, vector<string> *families);
 
+/**
+ * rowkey, column family, column qualifier 를 지정하여 get 한다.
+ *
+ * @param rowkeys rowkey 목록
+ * @param families column family 목록
+ * @param qualifiers qualifier 목록
+ */
 void gets(vector<string> *rowkeys, vector<string> *families, vector<string> *qualifiers);
