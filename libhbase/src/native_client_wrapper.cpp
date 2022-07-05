2e27ebe5f83b2788f5aa322d88c90ad223080af0
HaNeul-Kim <hskimsky@gmail.com>
2022-07-05 21:38:04 +0900

    endl 추가

--- libhbase/src/native_client_wrapper.cpp
+++ libhbase/src/native_client_wrapper.cpp
@@ -22,22 +22,22 @@ vector<string> split(string input, char delimiter) {
 
 void hbase_gets(vector<string> rowkeys, vector<string> families) {
     for (int i = 0; i < rowkeys.size(); i++) {
-        cout << "rowkeys[" << i << "] = " << rowkeys[i] << " ";
+        cout << "rowkeys[" << i << "] = " << rowkeys[i] << " " << endl;
     }
     for (int i = 0; i < families.size(); i++) {
-        cout << "families[" << i << "] = " << families[i] << " ";
+        cout << "families[" << i << "] = " << families[i] << " " << endl;
     }
 }
 
 void hbase_gets(vector<string> rowkeys, vector<string> families, vector<string> qualifiers) {
     for (int i = 0; i < rowkeys.size(); i++) {
-        cout << "rowkeys[" << i << "] = " << rowkeys[i] << " ";
+        cout << "rowkeys[" << i << "] = " << rowkeys[i] << " " << endl;
     }
     for (int i = 0; i < families.size(); i++) {
-        cout << "families[" << i << "] = " << families[i] << " ";
+        cout << "families[" << i << "] = " << families[i] << " " << endl;
     }
     for (int i = 0; i < qualifiers.size(); i++) {
-        cout << "qualifiers[" << i << "] = " << qualifiers[i] << " ";
+        cout << "qualifiers[" << i << "] = " << qualifiers[i] << " " << endl;
     }
 }
 
@@ -45,12 +45,9 @@ int main(int argc, char **argv) {
     const char *rowkeys_arg = (argc > 1) ? argv[1] : "rowkey0,rowkey1";
     const char *cfs_arg = (argc > 2) ? argv[2] : "test_cf1,test_cf2";
     const char *qs_arg = (argc > 3) ? argv[3] : "test_q1,test_q2,test_q5,test_q22";
-//    const char *zk_ensemble = (argc > 2) ? argv[2] : "adm1.hdp.io:2181,hdm1.hdp.io:2181,hdm2.hdp.io:2181";
-//    const char *zk_root_znode = (argc > 3) ? argv[3] : "/hbase-unsecure";
     std::string rowkeys(rowkeys_arg);
     std::string cfs(cfs_arg);
     std::string qs(qs_arg);
-    cout << "rowkeys = " << rowkeys << endl;
     const vector<string> rowkey_vector = split(rowkeys, ',');
     const vector<string> cf_vector = split(cfs, ',');
     const vector<string> q_vector = split(qs, ',');
