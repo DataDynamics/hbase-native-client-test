993fe640101030edfea9a95ac7e6e329dddcfa2b
HaNeul-Kim <hskimsky@gmail.com>
2022-07-05 19:42:12 +0900

    main 추가

--- libhbase/src/native_client_wrapper.cpp
+++ libhbase/src/native_client_wrapper.cpp
@@ -7,3 +7,11 @@ using namespace std;
 void print_value(int x) {
     cout << x << endl;
 }
+
+int main(int argc, char **argv) {
+  const char *x = (argc > 1) ? argv[1] : "111";
+//  const char *zk_ensemble = (argc > 2) ? argv[2] : "adm1.hdp.io:2181,hdm1.hdp.io:2181,hdm2.hdp.io:2181";
+//  const char *zk_root_znode = (argc > 3) ? argv[3] : "/hbase-unsecure";
+  int i = std::stoi(int_val);
+  print_value(i);
+}
