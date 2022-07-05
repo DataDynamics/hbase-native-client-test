b78fb79d6f90e11184e5b8591b3fe56731b9111c
HaNeul-Kim <hskimsky@gmail.com>
2022-07-05 19:51:45 +0900

    string to int test

--- libhbase/src/native_client_wrapper.cpp
+++ libhbase/src/native_client_wrapper.cpp
@@ -13,6 +13,7 @@ int main(int argc, char **argv) {
 //  const char *zk_ensemble = (argc > 2) ? argv[2] : "adm1.hdp.io:2181,hdm1.hdp.io:2181,hdm2.hdp.io:2181";
 //  const char *zk_root_znode = (argc > 3) ? argv[3] : "/hbase-unsecure";
   std::string str(arg1);
+  cout << "str = " << str << endl;
   int i = std::stoi(str);
   print_value(i);
 }
