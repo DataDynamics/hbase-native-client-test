ef2c2fd8e8e2505a29683f2f71ced60c9c383b0d
HaNeul-Kim <hskimsky@gmail.com>
2022-07-07 00:36:59 +0900

    get api compile test

--- libhbase/build/build.sh
+++ libhbase/build/build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 rm -rf  CMakeFiles/ CMakeCache.txt cmake_install.cmake libhbase_wrapper liblibhbaseclient.so Makefile
 cmake .. && make
+# export HBASE_CONF_DIR=/etc/hbase/conf
 # export CLASSPATH=$(echo /home/ddadmin/Downloads/hbase-libs-hdp/* | tr ' ' ':')
 # ./libhbase_wrapper
