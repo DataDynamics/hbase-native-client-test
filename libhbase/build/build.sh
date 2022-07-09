73afdca1e7107d480d624df1084a4a5e3de24f73
HaNeul-Kim <hskimsky@gmail.com>
2022-07-09 18:40:21 +0900

    compile test 정리

--- libhbase/build/build.sh
+++ libhbase/build/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm -rf  CMakeFiles/ CMakeCache.txt cmake_install.cmake libhbase_wrapper liblibhbaseclient.so Makefile
 cmake .. && make
+# for libhbase
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/libhbase/lib/so:${JAVA_HOME}/jre/lib/amd64/server
 # export HBASE_CONF_DIR=/etc/hbase/conf
-# export CLASSPATH=$(echo /home/ddadmin/Downloads/hbase-libs-hdp/* | tr ' ' ':')
-# export LD_LIBRARY_PATH=/home/ddadmin/Downloads/libhbase-dd/target/libhbase-1.0-SNAPSHOT/lib/native
-# ./libhbase_wrapper
+# export CLASSPATH=${CLASSPATH}:$(echo /usr/hdp/current/hbase-client/lib/* | tr ' ' ':'):/usr/hdp/current/hadoop-client/hadoop-common.jar:/usr/hdp/current/hadoop-client/hadoop-auth.jar:$(echo ${HOME}/libhbase/lib/jar/* | tr ' ' ':')
